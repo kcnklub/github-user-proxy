5a0a83493144c96dd25e1834707e8b5263f055cc
kymiller <kylemiller457@gmail.com>
2025-11-17 20:35:21 -0600

    init commit

---
+++ src/main/java/miller/kyle/github_user_proxy/GithubUserProxyApplication.java
@@ -0,0 +1,13 @@
+package miller.kyle.github_user_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubUserProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/miller/kyle/github_user_proxy/GithubUserProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package miller.kyle.github_user_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUserProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
