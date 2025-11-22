fa811180dc2df504f37e2e5b17e71a5bfcdb0e43
kymiller <kylemiller457@gmail.com>
2025-11-22 16:34:37 -0600

    Fixing tests in pipelines around test containers

--- src/test/java/miller/kyle/github_user_proxy/integration/UserProxyIntegrationTest.java
+++ src/test/java/miller/kyle/github_user_proxy/integration/UserProxyIntegrationTest.java
@@ -29,9 +29,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
 class UserProxyIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static GenericContainer<?> redis = new GenericContainer<>("redis:7-alpine")
+            .withExposedPorts(6379);
 
     @Autowired
     private MockMvc mockMvc;
